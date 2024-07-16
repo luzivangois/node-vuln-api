96aa52797b8f76c2849c3389886034b69242f367
Luzivan Gois <luzivangois@outlook.com>
2024-07-15 23:45:30 -0300

    Refatorado as rotas e a porta de deploy da aplicação e criada política de CORS para adaptação ao frontend advocacia-associados

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const userRoutes = require('./routes/userRoutes')
+const cors = require('cors')
 
 const app = express()
-const PORT = 3000
+const PORT = 8080
 
+app.use(cors({
+  origin: 'http://localhost:4200',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/api/users', userRoutes)
+app.use('/auth', userRoutes)
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
