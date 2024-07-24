e719528ecdbbc7ab105a698b9dbc22b3cf410afa
Luzivan Gois <luzivangois@outlook.com>
2024-07-23 23:24:19 -0300

    Criado o endpoint de consultar dados do usuário por ID

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -33,7 +33,7 @@ exports.login = async (req, res) => {
     } else if (findUser) {
       const token = jwt.sign({ id: user._id, sub: user.login }, SECRET_KEY, { expiresIn: 600 })
       res.send({
-        message: "Login Realizado com Sucesso",
+        message: 'Login Realizado com Sucesso',
         id: findUser._id,
         login: findUser.login,
         token: token
@@ -46,11 +46,31 @@ exports.login = async (req, res) => {
   }
 }
 
+exports.userId = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const user = await User.findById(id)
+
+    if (!user) {
+      return res.status(404).send({ message: 'Usuário não encontrado!'})
+    } else {
+      return res.status(200).send({
+        message: 'Usuário encontrado com Sucesso!',
+        id: user._id,
+        login: user.login,
+        password: user.password
+      })    }
+  } catch (err) {
+    res.status(500).send({ message: 'Erro ao buscar usuários!' })
+  }
+}
+
 exports.allUsers = async (req, res) => {
   const token = req.header('Authorization')
 
   if (!token) {
-    return res.status(401).send({ message: "Necessário Autenticação"})
+    return res.status(401).send({ message: 'Necessário Autenticação'})
   }
 
   try {
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController')
 
 router.post('/register', userController.register)
 router.post('/login', userController.login)
+router.get('/userid/:id', userController.userId)
 router.get('/allusers', userController.allUsers)
 
 module.exports = router
